0c61d76ba4c591e8fcf2dd356c0dfcc32cbaa484
user <user@LAPTOP-K1LVTM6V>
2022-08-08 14:51:51 +0800

    加入專案檔案。

---
+++ Form1.cs
@@ -0,0 +1,65 @@
+namespace drawing_app
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        int x = -1;
+        int y = -1;
+        bool moving = false;
+        Pen pen;
+        public Form1()
+        {
+            InitializeComponent();
+            g = panel1.CreateGraphics();
+            pen = new Pen(Color.Black,5);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+            pen.Color = p.BackColor;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+            pen.Color = p.BackColor;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+            pen.Color = p.BackColor;
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+            pen.Color = p.BackColor;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            moving = true;
+            x  = e.X;   
+            y = e.Y;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(moving && x!=1 && y != -1)
+            {
+                g.DrawLine(pen, new Point(x, y), e.Location);
+                x = e.X; //back to right shape
+                y=e.Y;
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            moving = false;
+            x = -1;
+            y = -1;
+        }
+    }
+}
